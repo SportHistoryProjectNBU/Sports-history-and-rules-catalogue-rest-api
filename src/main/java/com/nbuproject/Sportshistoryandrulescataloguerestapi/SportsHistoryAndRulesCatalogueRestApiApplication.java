ef0d4ca3fa164fe2e1d70e7ca3015c3b2f614645
Nikola Lefterov <crazyelemen7@abv.bg>
2017-10-27 20:36:33 +0300

    Initialize commit

---
+++ src/main/java/com/nbuproject/Sportshistoryandrulescataloguerestapi/SportsHistoryAndRulesCatalogueRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.nbuproject.Sportshistoryandrulescataloguerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsHistoryAndRulesCatalogueRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsHistoryAndRulesCatalogueRestApiApplication.class, args);
+	}
+}
